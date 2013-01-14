4ab1657075525693f761b9aea23d77c59c71940f
pablitomes <pablitomes@904b8c3e-1b29-c575-7119-250dbedbeb90>
2013-01-14 04:22:20 +0000

    Desarrollo

---
+++ Desarrollo/TMD_INTEGRADO/TMD.SIG/Entidades/GEN/OrganizacionE.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entidades.GEN
+{
+    public class OrganizacionE
+    {
+        private String CODIGO_ORGANIZACION;
+        private string NOMBRE;
+        private string VISION;
+        private string MISION;
+
+        public void setCODIGO_ORGANIZACION(String CODIGO_ORGANIZACION)
+        {
+            this.CODIGO_ORGANIZACION = CODIGO_ORGANIZACION;
+        }
+
+        public String getCODIGO_ORGANIZACION()
+        {
+            return CODIGO_ORGANIZACION;
+        }
+
+        public void setNOMBRE(string NOMBRE)
+        {
+            this.NOMBRE = NOMBRE;
+        }
+
+        public string getNOMBRE()
+        {
+            return NOMBRE;
+        }
+
+        public void setMISION(string MISION)
+        {
+            this.MISION = MISION;
+        }
+
+        public string getMISION()
+        {
+            return MISION;
+        }
+
+        public void setVISION(string VISION)
+        {
+            this.VISION = VISION;
+        }
+
+        public string getVISION()
+        {
+            return VISION;
+        }
+
+    }
+}
